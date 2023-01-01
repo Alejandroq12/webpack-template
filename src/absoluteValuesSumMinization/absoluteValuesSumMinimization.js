0d7d310dd2d3c2fb091ec59b42c6c34bad724007
Julio Quezada <julio.quezada@catolica.edu.sv>
2022-12-31 23:57:48 -0600

    Update absoluteValuesSumMinimization.js

--- src/absoluteValuesSumMinization/absoluteValuesSumMinimization.js
+++ src/absoluteValuesSumMinization/absoluteValuesSumMinimization.js
@@ -1,3 +1,4 @@
+// Ejercicio con Javascript
 function absoluteValuesSumMinimization(a) {
     const isEven = a.length % 2 === 0; 
     return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length /2)];
