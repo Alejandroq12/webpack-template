217e3962ca2f6645db55e4da0a920f10a60bb213
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-03-29 11:36:13 -0600

    Add configuration to include a .css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: { 
-    index: './src/index.js'
+  entry: {
+    index: "./src/index.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
